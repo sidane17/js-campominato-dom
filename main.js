fe3b5f65205f10a03ec94415c6032bd6be14d075
sidane17 <khalilsidane89@gmail.com>
2024-05-03 11:42:01 +0200

    bomba/finito

--- main.js
+++ main.js
@@ -6,35 +6,91 @@ let num;
 let e;
 let m;
 let h;
+let w=0;
+let random=[];
+let r=0;
+let k;
+
+
+// ---------------------------------------------------------------------------------------------
+function casuale(num){
+    while(w<16){
+        r=Math.floor(Math.random() * (num) + 1);
+       
+        if(random.includes(r)==false){
+            random[w]=r;
+            w++;
+        }
+    }
+    return random
+}
+
+
 // ---------------------------------------------------------------------------------------------
 
 easy.onclick= function(){
      e=easy.innerHTML;
      e=parseInt(e);
     num=e;
-    console.log(num);
+    k=casuale(num);
+    console.log(k);
+    console.log(k)
+
 }
 medium.onclick= function(){
     m=medium.innerHTML;
     m=parseInt(m);
     num=m;
+    k=casuale(num);
+    console.log(k)
 }
 hard.onclick= function(){
     h=hard.innerHTML;
     h=parseInt(h);
     num=h;
+    k=casuale(num);
+    console.log(k)
+    
+    
 }
+
+
 // ---------------------------------------------------------------------------------------------
 
 document.getElementById("gioca").onclick=function(){
     let b =document.getElementById("container_game");
-    
-for(let i=1;i<=num;i++){
-    let elemento=creaElemento(num,i)
-    b.append(elemento);
-    elemento.addEventListener("click",function(){
-        console.log(i);
-        elemento.classList.toggle("background");
+    let win=0;
+    let lose=0;
+    for(let i=1;i<=num;i++){
+        let elemento=creaElemento(num,i)
+        b.append(elemento);
+        elemento.addEventListener("click",function(){
+            let z=elemento.innerHTML;
+            z=parseInt(z);
+           
+            
+            if(k.includes(z)==true){
+                console.log("click")
+            elemento.classList.toggle("background"); 
+            elemento.innerHTML="";
+            
+            lose++;
+            
+            if(lose==16){
+                alert("hai perso")
+            }
+            
+            }else {
+                elemento.classList.toggle("background_win");
+                win++;
+
+                if(win==(num-16)){
+                    alert("hai vinto")
+                }
+            }
+
+        
+            
     })
 }
 // ---------------------------------------------------------------------------------------------
